a2070a8201337991212be8f5b0ee55f50712068c
osamaaslam86004 <osama.aslam.86004@gmail.com>
2025-02-05 22:25:49 +0500

    nothing

--- iii/settings.py
+++ iii/settings.py
@@ -150,13 +150,13 @@ if DEBUG:
             },
         }
     }
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
+# else:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": BASE_DIR / "db.sqlite3",
+#         }
 #     }
-# }
 # else:
 #     DATABASES = {
 #         "default": {
