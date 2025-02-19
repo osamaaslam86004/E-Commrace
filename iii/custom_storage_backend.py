c5f7051e5bfac7b1f4a05d019fb39ffdb18c3647
osamaaslam86004 <osama.aslam.86004@gmail.com>
2025-02-20 00:11:57 +0500

    file_overwrite set to False in custom_storage_backend.py

--- iii/custom_storage_backend.py
+++ iii/custom_storage_backend.py
@@ -10,7 +10,7 @@ GB = 1024 * 1024
 class StaticStorage(S3ManifestStaticStorage):
     location = "static"
     default_acl = None
-    file_overwrite = True  # Allows overwriting old static files
+    file_overwrite = False  # Allows overwriting old static files
     transfer_config = TransferConfig(use_threads=False, multipart_threshold=5 * GB)
 
 
@@ -18,5 +18,5 @@ class PublicMediaStorage(S3Boto3Storage):
     # 1. Your PublicMediaStorage class should explicitly set the default_acl to "public-read" to allow direct access to files without signing.
     location = "media"
     default_acl = None
-    file_overwrite = True
+    file_overwrite = False
     transfer_config = TransferConfig(use_threads=False, multipart_threshold=5 * GB)
